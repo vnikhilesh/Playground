c6df2694bc7b3655614cface14730270c05a0fb9
vnikhilesh <64413011+vnikhilesh@users.noreply.github.com>
2020-04-29 10:47:59 +0530

    Test code pushed to repository.

---
+++ Its result time/Main.java
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+int id;
+int marks;
+  std::cin>>id;
+  std::cin>>marks;
+  std::cout<<"Id : "<<id;
+  std::cout<<"\n";
+  std::cout<<"Totalmarks : "<<marks;
+}  
+    
+  
+  
---
+++ Naming  Ceremony/Main.java
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+char name[8];
+int i;
+  for(i=0;i<8;i++)
+  {  
+  std::cin>>name;
+  }
+  std::cout<<name;
+}
+    
